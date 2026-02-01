056be53cade8796105741a299a052da890d1a46a
azhgh22 <azhgh22@freeuni.edu.ge>
2026-02-01 17:30:36 +0000

    modified train

--- utils/train.py
+++ utils/train.py
@@ -8,6 +8,7 @@ class Train:
         val_loader,
         optimizer,
         criterion,
+        eval_list = [],
         device="cpu"
     ):
         self.model = model.to(device)
@@ -16,20 +17,21 @@ class Train:
         self.optimizer = optimizer
         self.criterion = criterion
         self.device = device
+        self.eval_list = eval_list
 
     def train_epoch(self):
         self.model.train()
         total_loss = 0
-        correct = 0
         total = 0
+        epoch_output = []
 
         for x, y in self.train_loader:
             x = x.to(self.device)
             y = y.float().to(self.device)
 
-            # Forward (already probability)
-            probs = self.model(x)              # (B, 1) in [0,1]
-            loss = self.criterion(probs, y)
+            out = self.model(x)
+            loss = self.criterion(out, y)
+            epoch_output.append((out,y))
 
             # Backward
             self.optimizer.zero_grad()
@@ -38,42 +40,42 @@ class Train:
 
             total_loss += loss.item()
 
-            preds = (probs > 0.5).int()
-            correct += (preds == y.int()).sum().item()
             total += y.size(0)
 
-        return total_loss / len(self.train_loader), correct / total
+        return total_loss / len(self.train_loader), epoch_output
 
     def val_epoch(self):
         self.model.eval()
         total_loss = 0
-        correct = 0
         total = 0
+
+        epoch_output = []
 
         with torch.no_grad():
             for x, y in self.val_loader:
                 x = x.to(self.device)
                 y = y.float().to(self.device)
 
-                probs = self.model(x)
-                print(probs.shape,y.shape)
-                loss = self.criterion(probs, y)
+                out = self.model(x)
+                loss = self.criterion(out, y)
+                epoch_output.append((out,y))
 
                 total_loss += loss.item()
-
-                preds = (probs > 0.5).int()
-                correct += (preds == y.int()).sum().item()
                 total += y.size(0)
 
-        return total_loss / len(self.val_loader), correct / total
+        return total_loss / len(self.val_loader), epoch_output
 
     def train(self, num_epochs):
         for epoch in range(num_epochs):
-            train_loss, train_acc = self.train_epoch()
-            val_loss, val_acc = self.val_epoch()
+            train_loss, train_out = self.train_epoch()
+            val_loss,val_out = self.val_epoch()
 
             print(
                 f"Epoch {epoch+1}/{num_epochs} | "
-                f"Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f} | "
-                f"Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}"
+                f"Train Loss: {train_loss:.4f} | "
+                f"Val Loss: {val_loss:.4f}"
             )
+
+            for f in self.eval_list:
+                f.evaluate("Train: ",train_out)
+                f.evaluate("Val: ",val_out)
