215ce0b6fcd9f1685223f8a4df7cd8a373af7974
azhgh22 <azhgh22@freeuni.edu.ge>
2026-02-01 16:20:19 +0000

    bla

--- utils/data_split.py
+++ utils/data_split.py
@@ -1,15 +1,40 @@
-import torch
-from torch.utils.data import random_split
+import numpy as np
+from torch.utils.data import Subset
 
+def dataset_split(dataset, train_ratio=0.7, val_ratio=0.15, test_ratio=0.15, seed=42):
+    assert train_ratio + val_ratio + test_ratio == 1.0
 
-def test_train_split(dataset,train_p,val_p,random_seed=42):
-  total_size = len(dataset)
-  train_size = int(train_p * total_size)
-  val_size   = int(val_p * total_size)
-  test_size  = total_size - train_size - val_size
+    labels = np.array(dataset.labels)
 
-  generator = torch.Generator().manual_seed(42)
-  train_dataset, val_dataset, test_dataset = random_split(
-    dataset, [train_size, val_size, test_size]
-  )
-  return train_dataset,val_dataset,test_dataset
+    idx_pos = np.where(labels == 1)[0]
+    idx_neg = np.where(labels == 0)[0]
+
+    rng = np.random.default_rng(seed)
+    rng.shuffle(idx_pos)
+    rng.shuffle(idx_neg)
+
+    def split_indices(idxs):
+        n = len(idxs)
+        n_train = int(train_ratio * n)
+        n_val   = int(val_ratio * n)
+        train = idxs[:n_train]
+        val   = idxs[n_train:n_train+n_val]
+        test  = idxs[n_train+n_val:]
+        return train, val, test
+
+    pos_train, pos_val, pos_test = split_indices(idx_pos)
+    neg_train, neg_val, neg_test = split_indices(idx_neg)
+
+    train_idx = np.concatenate([pos_train, neg_train])
+    val_idx   = np.concatenate([pos_val, neg_val])
+    test_idx  = np.concatenate([pos_test, neg_test])
+
+    rng.shuffle(train_idx)
+    rng.shuffle(val_idx)
+    rng.shuffle(test_idx)
+
+    return (
+        Subset(dataset, train_idx),
+        Subset(dataset, val_idx),
+        Subset(dataset, test_idx)
+    )
